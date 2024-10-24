52106d5f82ba5df18a9ca9663b9a4876a1a8c617
zxc1234321 <spcle2412@gmail.com>
2024-10-25 05:58:10 +0900

    Update: 회원가입 구현

--- README.md
+++ README.md
@@ -43,8 +43,8 @@ YOLOv8
 ---
 
 # 개발 기간
-- 2024.03 ~
-=======
+- 2024.03 ~ 2024.10
+---
 <p align="center">
   <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
 </p>
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [`${__dirname}/**/*.entity.{ts,js}`],
+      synchronize: process.env.DB_SYNC === 'true',
+    }),
+    AuthModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
